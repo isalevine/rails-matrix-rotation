4363b9f9c281b1b9159de9b9fdc325e036d80948
isalevine <isa.loves.coding@gmail.com>
2019-10-23 23:45:00 -0700

    added rest-client, building api queries

--- magic-art.rb
+++ magic-art.rb
@@ -1,1 +1,13 @@
-puts "hi"
+require_relative './config/environment.rb'
+
+def get_json(url)
+    response = RestClient.get(url)
+    json = JSON.parse(response)
+  end
+
+def scryfall_api
+    json = get_json('https://api.scryfall.com/cards/search?q=t%3Alegend')
+    puts json
+end
+
+scryfall_api
