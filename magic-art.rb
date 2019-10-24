a411c83f664797485a381c002c1d61163944784a
isalevine <isa.loves.coding@gmail.com>
2019-10-23 23:58:09 -0700

    now printing 1st page of art_crop

--- magic-art.rb
+++ magic-art.rb
@@ -3,11 +3,21 @@ require_relative './config/environment.rb'
 def get_json(url)
     response = RestClient.get(url)
     json = JSON.parse(response)
-  end
+end
+
+def parse_cards(json)
+    array = json["data"]
+    array.each do |card_hash|
+        if card_hash["image_uris"]
+            puts card_hash["image_uris"]["art_crop"]
+        end
+    end
+end
 
 def scryfall_api
     json = get_json('https://api.scryfall.com/cards/search?q=t%3Alegend')
-    puts json
+    parse_cards(json)
+    # puts json
 end
 
 scryfall_api
